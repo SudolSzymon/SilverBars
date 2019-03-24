85ea5d8e0728dd8cf9a0ad45bc3456d31927c9ef
SzymonSudol <szymonsudol@gmail.com>
2019-03-24 22:04:36 +0000

    Silver bars project initial commit

---
+++ src/LiveOrderBoard.java
@@ -0,0 +1,65 @@
+import java.util.*;
+
+public class LiveOrderBoard {
+
+    //To keep orders used maps between orders at certain price and price to make it  fast and easy to  sum quantity for each  price as requested
+    //used tree map to keep entries sorted
+    private SortedMap<Double, List<Order>> buyOrders = new TreeMap<>((e1, e2) -> {
+        if (e1.equals(e2))
+            return 0;
+        return e1 > e2 ? -1 : 1;
+    });
+    //Sell orders and buy  orders maps are separated to make summary generation faster
+    private SortedMap<Double, List<Order>> sellOrders = new TreeMap<>((e1, e2) -> {
+        if (e1.equals(e2))
+            return 0;
+        return e1 > e2 ? 1 : -1;
+    });
+
+    public void registerOrder(String userID, int orderQuantity, double pricePerKg, OrderType type) {
+        SortedMap<Double, List<Order>> orders = type == OrderType.BUY ? buyOrders : sellOrders;
+        List<Order> ordersList = orders.get(pricePerKg);
+        boolean isNewOrderList = false;
+        if (ordersList == null) {
+            ordersList = new ArrayList<>();
+            isNewOrderList = true;
+        }
+        ordersList.add(new Order(userID, orderQuantity, pricePerKg, type));
+        if(isNewOrderList)  // No need to do put it to map if it is already in the list as list will updated saves time
+            orders.put(pricePerKg, ordersList);
+    }
+
+    public boolean cancelOrder(String userID, int orderQuantity, double pricePerKg, OrderType type) {
+        SortedMap<Double, List<Order>> orders = type == OrderType.BUY ? buyOrders : sellOrders;
+        List<Order> ordersList = orders.get(pricePerKg);
+        if (ordersList == null) {
+            return false;
+        }
+        boolean result = ordersList.remove(new Order(userID, orderQuantity, pricePerKg, type));
+        if (ordersList.isEmpty()) {
+            orders.remove(pricePerKg);
+        }
+        return result;
+    }
+
+    // Summary takes type as argument as depending on type those need to be sorted differently
+    public String getSummary(OrderType type) {
+        SortedMap<Double, List<Order>> orders = type == OrderType.BUY ? buyOrders : sellOrders;
+        StringBuilder builder = new StringBuilder();
+        for (Map.Entry<Double, List<Order>> entry : orders.entrySet()) {
+            long totalQuantityPerPrice = 0;
+            for (Order order : entry.getValue()) {
+                totalQuantityPerPrice += order.getQuantity();
+            }
+            //format is as follows [1112Kg for 11.0£ per Kg]
+            builder.append("[").append(totalQuantityPerPrice).append("Kg for ").append(entry.getKey()).append("£ per Kg]");
+        }
+        return builder.toString();
+    }
+
+
+
+
+}
+
+
---
+++ src/Order.java
@@ -0,0 +1,57 @@
+import static java.util.Objects.requireNonNull;
+
+public class Order {
+
+    // Decided to use simple int instead of  Quantity class as unit is always Kg if it can be different should crate class to keep unit as well and provide some  unit conversion
+    private int quantity;
+    private String userID;
+    private OrderType type;
+    // Decided to use simple double instead of Price class as unit is always GBP if it can be different should crate class to keep currency as well and provide some  currency conversion
+    private double pricePerKg;
+
+    public Order(String userID, int quantity, double price, OrderType type) {
+        //require all these field to be  not nul  as are needed for  valid order
+        requireNonNull(userID, "user id");
+        requireNonNull(price, "price per Kg");
+        requireNonNull(type, "type of order");
+        requireNonNull(quantity, "quantity");
+        this.type = type;
+        this.userID = userID;
+        this.quantity = quantity;
+        this.pricePerKg = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if (!(other instanceof Order)) {
+            return false;
+        }
+        Order otherOrder = (Order) other;
+        if (!otherOrder.userID.equals(userID))
+            return false;
+        if (!(otherOrder.type == type))
+            return false;
+        if (!(otherOrder.quantity == quantity))
+            return false;
+        if (!(otherOrder.pricePerKg == pricePerKg))
+            return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + userID.hashCode();
+        result = 37 * result + quantity;
+        result = 41 * result + Double.valueOf(pricePerKg).hashCode();
+        result = 43 * result + (type == OrderType.BUY ? 1 : 2);
+        return result;
+    }
+}
